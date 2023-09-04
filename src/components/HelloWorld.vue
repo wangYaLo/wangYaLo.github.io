451e2c6885c7f3e5aea8f9e31088693fbe56824e
wangYaLo <duoyuan7225@163.com>
2023-09-04 15:40:01 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">主席是傻逼</button> {{ count }}</button>
+    <button type="button" @click="count++">主席是傻逼 {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
