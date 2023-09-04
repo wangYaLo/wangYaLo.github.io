382e2ccfa67494a6f7f570b2b2ab4d927f85665d
wangYaLo <duoyuan7225@163.com>
2023-09-04 15:28:00 +0800

    创建项目

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 自动打包配置
+
+set -x  # 这里是为了看错误日志
+
+# 打包项目
+npm run build
+
+# 进入打包后的文件夹
+cd dist
+
+git init
+git add -A
+git commit -m 'auto deploy'
+
+# 将打包后的文件推送到指定分支
+
+git push -f https://github.com/wangYaLo/wangYaLo.github.io.git main:static-pages
